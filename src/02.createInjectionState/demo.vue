78c7a65b0736ac081320ab39aec529de69b24473
wangzhiyi <35094245+qq449245884@users.noreply.github.com>
2023-03-20 20:37:39 +0800

    feat: 完成

---
+++ src/02.createInjectionState/demo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import RootComponent from './RootComponent.vue'
+import CountComponent from './CountComponent.vue'
+import ButtonComponent from './ButtonComponent.vue'
+</script>
+
+<template>
+  <div>
+    <RootComponent>
+      <ButtonComponent />
+      <CountComponent />
+    </RootComponent>
+  </div>
+</template>
